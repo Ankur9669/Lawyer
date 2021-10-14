935024f5aabaa592cffdfe11e1f7f31fd6512023
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2021-10-14 19:03:41 +0530

    npm run build

--- lawyer/src/App.js
+++ lawyer/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import MainHeader from './components/MainHeader';
 import Navbar from './components/Navbar';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <MainHeader/>
       <About/>
+      <Services/>
     </div>
   );
 }
---
+++ lawyer/src/components/Services.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./services.css"
+function Services() {
+    return (
+        <div className = "our-services">
+            <h3 className = "our-services-heading">Our Services</h3>
+            <div className = "our-services-cards">
+                <div className = "our-services-card">
+                    <img src = "/" alt = "img"></img>
+                    <p className = "our-services-card-heading">Family Law</p>
+                    <p className = "our-services-card-text">Lorem ipsum dolor sit amet, consectetur a elit, sed do eiu tempor incididunt ute labore etrt dolore.</p>
+                </div>
+                <div className = "our-services-card">
+                    <img src = "/" alt = "img"></img>
+                    <p className = "our-services-card-heading">Family Law</p>
+                    <p className = "our-services-card-text">Lorem ipsum dolor sit amet, consectetur a elit, sed do eiu tempor incididunt ute labore etrt dolore.</p>
+                </div>
+                <div className = "our-services-card">
+                    <img src = "/" alt = "img"></img>
+                    <p className = "our-services-card-heading">Family Law</p>
+                    <p className = "our-services-card-text">Lorem ipsum dolor sit amet, consectetur a elit, sed do eiu tempor incididunt ute labore etrt dolore.</p>
+                </div>
+                <div className = "our-services-card">
+                    <img src = "/" alt = "img"></img>
+                    <p className = "our-services-card-heading">Family Law</p>
+                    <p className = "our-services-card-text">Lorem ipsum dolor sit amet, consectetur a elit, sed do eiu tempor incididunt ute labore etrt dolore.</p>
+                </div>
+                <div className = "our-services-card">
+                    <img src = "/" alt = "img"></img>
+                    <p className = "our-services-card-heading">Family Law</p>
+                    <p className = "our-services-card-text">Lorem ipsum dolor sit amet, consectetur a elit, sed do eiu tempor incididunt ute labore etrt dolore.</p>
+                </div>
+                <div className = "our-services-card">
+                    <img src = "/" alt = "img"></img>
+                    <p className = "our-services-card-heading">Family Law</p>
+                    <p className = "our-services-card-text">Lorem ipsum dolor sit amet, consectetur a elit, sed do eiu tempor incididunt ute labore etrt dolore.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services
